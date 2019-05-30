fdfb93552ba6a418b1d572178c9ebe8ac1420a11
Dhruvil Dave <dhruvil.dave@outlook.com>
2019-05-30 18:44:13 +0530

    Thu 30 May 2019 06:43:47 PM IST

---
+++ fish_first_setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/fish
+
+function codes
+	cd /mnt/4427FDEE206BF5AE/Documents/codes
+end
+funcsave codes
+
+function ref
+	cd /mnt/4427FDEE206BF5AE/Documents/codes/reference-books
+end
+funcsave ref
