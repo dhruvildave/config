00665ddaf06c19e422c733e91a7a222e37ca8183
Dhruvil Dave <43331416+dhruvildave@users.noreply.github.com>
2019-12-30 23:04:59 +0530

    Mon Dec 30 23:04:37 IST 2019

--- update.sh
+++ update.sh
@@ -3,13 +3,17 @@
 
 readonly dist="$(grep -w "ID" /etc/os-release | cut -d= -f2)"  # Find the name of distro
 
+yellow() { echo -e "\\nUpdating \\033[01;33m$1\\033[00m"; }  # Prints the argument in yellow color
+
 # Base package manager
 if [ "$dist" == "fedora" ] || [ "$dist" == "centos" ]; then
     if [ -x "$(command -v dnf)" ]; then
+        yellow dnf
         sudo dnf upgrade -y
     fi
 elif [ "$dist" == "debian" ] || [ "$dist" == "ubuntu" ]; then
     if [ -x "$(command -v apt)" ]; then
+        yellow apt
         sudo apt update
         sudo apt full-upgrade -y
         sudo apt autoremove -y
@@ -18,7 +22,7 @@ fi
 
 # Conda
 if [ -x "$(command -v conda)" ]; then
-    echo -e "\\nUpdating \\033[01;33mbase\\033[00m"
+    yellow "conda->base"
     conda update --all -y -n base
     if [ -d "$HOME/miniconda3/" ]; then
         for i in "$HOME/miniconda3/envs"/*; do
@@ -28,7 +32,7 @@ if [ -x "$(command -v conda)" ]; then
                 continue  # Do not update torch
             fi
 
-            echo -e "\\nUpdating \\033[01;33m$name\\033[00m"
+            yellow "conda->$name"
             conda update --all -y -n "$name"
         done
     fi
@@ -36,6 +40,9 @@ if [ -x "$(command -v conda)" ]; then
 fi
 
 # Flatpak
+yellow flatpak
 if [ -x "$(command -v flatpak)" ]; then
     flatpak update -y
 fi
+
+unset -f yellow
