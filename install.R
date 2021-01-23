bb0483d2a8a0ff032f4971f5b6801e17b9b4ac98
Dhruvil Dave <43331416+dhruvildave@users.noreply.github.com>
2021-01-23 23:53:55 +0530

    Sat Jan 23 11:53:52 PM IST 2021

---
+++ install.R
@@ -0,0 +1,16 @@
+#!/usr/bin/env Rscript
+
+install.packages(
+  c(
+    "tidyverse",
+    "plotly",
+    "r2d3",
+    "renv",
+    "styler",
+    "miniUI",
+    "roxygen2",
+    "usethis",
+    "pkgdown",
+    "data.table"
+  )
+)
