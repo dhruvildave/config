9906b8da45b7b3e18dc0598e90e40f1c00a097ad
Dhruvil Dave <43331416+dhruvildave@users.noreply.github.com>
2021-10-06 16:16:47 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,7 +4,8 @@
 # Mon Dec 30 23:14:13 IST 2019
 # Author: https://github.com/dhruvildave
 
-readonly dist="$(grep -w "ID" /etc/os-release | cut -d= -f2)" # Find the name of distro
+dist="$(grep -w "ID" /etc/os-release | cut -d= -f2)" # Find the name of distro
+readonly dist
 
 # Prints the argument in yellow color
 yellow() {
@@ -44,7 +45,8 @@ esac
         name="$(echo "$i" | rev | cut -d/ -f1 | rev)"
 
         yellow "conda->$name"
-        conda update --all -y -n "$name"
+        conda update --all -y -n "$name" \
+            -c conda-forge -c plotly -c pytorch
     done
 unset name
 
@@ -63,13 +65,20 @@ if [[ -x "$(command -v node | grep -w nvm)" ]]; then
             nvm uninstall "$ver" &&
                 nvm use node &&
                 npm i -g yarn pnpm &&
-                pnpm add -g typescript prettier nodemon serve parcel-bundler &&
+                pnpm add -g typescript prettier nodemon serve parcel &&
                 unset ver
         fi
 fi
 
-yellow "Podman" &&
-    [ -f "$HOME/Documents/update-podman.sh" ] &&
-    sudo "$HOME/Documents/update-podman.sh"
+[[ -x "$(command -v docker)" ]] &&
+    yellow "Docker" &&
+    for i in $(docker images | awk 'NR > 1 { print $1":"$2 }'); do
+        docker pull "$i"
+    done &&
+    docker system prune -f
+
+# yellow "Podman" &&
+#     [ -f "$HOME/Documents/update-podman.sh" ] &&
+#     sudo "$HOME/Documents/update-podman.sh"
 
 unset -f yellow
