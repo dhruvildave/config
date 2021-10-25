a6ef6d6f2f68c61645ccd39abdaca3ae5ee188a6
Dhruvil Dave <43331416+dhruvildave@users.noreply.github.com>
2021-10-25 17:01:34 +0530

    Update debian_setup.sh

--- debian_setup.sh
+++ debian_setup.sh
@@ -71,13 +71,13 @@ if [ -x "$(command -v curl)" ]; then
     ./Miniconda3-latest-Linux-x86_64.sh
 
     # VSCode setup
-    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-    sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
-    sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-    sudo apt install apt-transport-https
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+    sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+    sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+    rm -f packages.microsoft.gpg
     sudo apt update
-    sudo apt install code
-    
+    sudo apt install -y code
+
     curl -O https://raw.githubusercontent.com/dhruvildave/config/master/update.sh
     chmod 0755 update.sh
 else
