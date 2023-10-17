889a19c04d8b74d0e647ffb93c2b4a49a50f0b96
Dhruvil Dave <43331416+dhruvildave@users.noreply.github.com>
2023-10-17 15:03:08 -0500

    Tue Oct 17 08:03:05 PM UTC 2023

--- template.go
+++ template.go
@@ -8,10 +8,15 @@ import (
 
 var buf = bufio.NewReadWriter(bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout))
 
-func soln() {}
+func soln() {
+	var n int
+	fmt.Fscan(buf, &n)
+	fmt.Fprintln(buf, n)
+}
 
 func main() {
 	defer buf.Flush()
+
 	t := 1
 	fmt.Fscan(buf, &t) // comment this if only one test case
 	for i := 0; i < t; i++ {
