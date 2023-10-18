a8ce34c124af87755eb71054e13c1b9b242115f9
Dhruvil Dave <43331416+dhruvildave@users.noreply.github.com>
2023-10-18 17:30:18 -0500

    Wed Oct 18 10:30:15 PM UTC 2023

--- template.go
+++ template.go
@@ -2,26 +2,83 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
-var buf = bufio.NewReadWriter(
-	bufio.NewReader(os.Stdin),
-	bufio.NewWriter(os.Stdout),
-)
-
-func soln() {
-	var n int
-	fmt.Fscan(buf, &n)
-	fmt.Fprintln(buf, n)
+type buffer struct {
+	*bufio.ReadWriter
 }
+
+func (b *buffer) read(v any) {
+	s, _ := b.ReadString('\n')
+	y := strings.Fields(s)
+
+	switch x := v.(type) {
+	case *bool:
+		*x, _ = strconv.ParseBool(y[0])
+	case *int64:
+		*x, _ = strconv.ParseInt(y[0], 10, 64)
+	case *float64:
+		*x, _ = strconv.ParseFloat(y[0], 64)
+	case *string:
+		*x = y[0]
+	case *[]bool:
+		for i, v := range y {
+			(*x)[i], _ = strconv.ParseBool(v)
+		}
+	case *[]int64:
+		for i, v := range y {
+			(*x)[i], _ = strconv.ParseInt(v, 10, 64)
+		}
+	case *[]float64:
+		for i, v := range y {
+			(*x)[i], _ = strconv.ParseFloat(v, 64)
+		}
+	case *[]string:
+		for i, v := range y {
+			(*x)[i] = v
+		}
+	default:
+		panic("expected 'pointer' or 'pointer to slice' of bool | int64 | float64 | string")
+	}
+}
+
+func (b *buffer) write(v any) {
+	var y string
+
+	switch x := v.(type) {
+	case bool:
+		y = strconv.FormatBool(x)
+	case int64:
+		y = strconv.FormatInt(x, 10)
+	case float64:
+		y = strconv.FormatFloat(x, 'g', -1, 64)
+	case string:
+		y = x
+	default:
+		panic("expected bool | int64 | float64 | string")
+	}
+
+	buf.WriteString(y)
+}
+
+var buf buffer = buffer{
+	bufio.NewReadWriter(
+		bufio.NewReader(os.Stdin),
+		bufio.NewWriter(os.Stdout),
+	),
+}
+
+func soln() {}
 
 func main() {
 	defer buf.Flush()
 
 	t := 1
-	fmt.Fscan(buf, &t) // comment this if only one test case
+	buf.read(&t) // comment this if only one test case
+
 	for i := 0; i < t; i++ {
 		soln()
 	}
